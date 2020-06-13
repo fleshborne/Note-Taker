0f9952dcbddff303d27079936299d227398e456b
Fleshborne <blakeathompson94@gmail.com>
2020-06-13 16:05:27 -0400

    htmlRoutes have been set

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,9 +1,23 @@
 // install your packages which may include 'path', 'express'(router),
-
+const path = require("path");
+const router = require("express").Router();
 // get /notes route
-// sendFile()- directory, filename you want to display
+router.get(
+  "/notes",
+  ((req, res) = () => {
+    // sendFile()- directory, filename you want to display
+    res.sendFile(path.join(__dirname, "../public/notes.html"));
+  })
+);
 
 // get "*" route
-// sendFile()- directory, filename you want to display
+router.get(
+  "*",
+  ((req, res) = () => {
+    // sendFile()- directory, filename you want to display
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+  })
+);
 
 // export the router
+module.exports = router;
