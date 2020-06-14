28877c9b26a2755f3902b95509bdd95df75d2ee1
Fleshborne <blakeathompson94@gmail.com>
2020-06-14 00:06:28 -0400

    that way didn't seem any easier

---
+++ Develop/db/Notebook.js
@@ -0,0 +1,25 @@
+// require uuidv4 package to generate an { id : uuidv4(), title,text  }
+const { uuid } = require("uuidv4");
+const fs = require("fs");
+const db = require("./db.json");
+const path = require("path");
+
+class Notebook {
+  constructor() {
+    this.notes = db;
+  }
+  getNotes() {
+    return this.notes;
+  }
+  // inside of this function, you're going to want READ from the db.json
+  // the contents of the json file will be displayed on the page
+  // to read a file - readFile() which is apart of the "fs" package
+  // addNotes()
+  // inside of this function, you want to use writeFile() from 'fs'
+  // deleteNotes()
+  // check against all of the notes to see which on has the id you're looking to delete
+  // call getNotes() and then filter the results to find the id you're looking for and return
+  // the ones that don't match
+}
+
+module.exports = new Notebook();
