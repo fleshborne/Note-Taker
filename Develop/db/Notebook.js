b72c2649b7f130ca89dcb3e9950ae2222fcf4f0a
Fleshborne <blakeathompson94@gmail.com>
2020-06-17 17:50:58 -0400

    reworked to include notebook class, followed along with the review

--- Develop/db/Notebook.js
+++ Develop/db/Notebook.js
@@ -1,49 +1,62 @@
+const util = require("util");
+const fs = require("fs");
 // require uuidv4 package to generate an { id : uuidv4(), title,text  }
-// const { uuid } = require("uuidv4");
-// const fs = require("fs");
-// const db = require("./db.json");
-// const path = require("path");
-// const util = require("util");
-// const readFileSync = util.promisify(fs.readFile);
-// const writeFileSync = util.promisify(fs.writeFile);
-// class Notebook {
-//   constructor(id, title, text) {
-//     this.id = id;
-//     this.title = title;
-//     this.text = text;
-//   }
-//   getNotes() {
-//     // inside of this function, you're going to want READ from the db.json
-//     // the contents of the json file will be displayed on the page
-//     // to read a file - readFile() which is apart of the "fs" package
-//     return readFileSync("db/db.json").then((notes) => {
-//       let newNotes = [];
-//       try {
-//         newNotes = [].concat(JSON.parse(notes));
-//       } catch (err) {
-//         newNotes = [];
-//       }
-//       return newNotes;
-//     });
-//   }
+const { uuid } = require("uuidv4");
 
-//   addNotes(note) {
-//     const { title, text } = note;
-//     const newNote = { id: uuid(), title, text };
-//     // inside of this function, you want to use writeFile() from 'fs'
-//     return this.getNotes()
-//       .then((notes) => [notes, newNote])
-//       .then((newNotes) => writeFileSync("db/db.json", JSON.stringify(newNotes)))
-//       .then(() => newNote);
-//   }
+const readFileSync = util.promisify(fs.readFile);
+const writeFileSync = util.promisify(fs.writeFile);
+class Notebook {
+  read() {
+    // this will return notes, utf8 is default but we will call it regardless for encoding
+    return readFileSync("db/db.json", "utf8");
+  }
+  write(note) {
+    //   allows for notes to be written to the db.json in string form
+    return writeFileSync("db/db.json", JSON.stringify(note));
+  }
 
-//   deleteNotes(id) {
-//     getNotes();
-//   }
+  getNotes() {
+    // inside of this function, you're going to want READ from the db.json
+    // the contents of the json file will be displayed on the page
+    // to read a file - readFile() which is apart of the "fs" package
+    return this.read().then((notes) => {
+      let newNotes;
+      try {
+        newNotes = [].concat(JSON.parse(notes));
+      } catch (err) {
+        newNotes = [];
+        // tutor said this is a good way to catch the error in case the array doesn't exist
+      }
+      return newNotes;
+    });
+  }
 
-//   // check against all of the notes to see which on has the id you're looking to delete
-//   // call getNotes() and then filter the results to find the id you're looking for and return
-//   // the ones that don't match
-// }
+  addNotes(note) {
+    const { title, text } = note;
+    // cited from review, validation on the back end is good practice
+    if (!title || !text) {
+      throw new Error("Note 'title' and 'text' cannot be blank");
+    }
+    // assigns content of notes
+    const newNote = { title, text, id: uuid() };
+    // inside of this function, you want to use writeFile() from 'fs'
+    return this.getNotes()
+      .then((notes) => [...notes, newNote])
+      .then((updatedNotes) => this.write(updatedNotes))
+      .then(() => newNote);
+    // here we're returning new notes written, saving them to the pre-existing array
+    // then writes the new note to the db/.json file
+  }
 
-// module.exports = new Notebook();
+  deleteNotes(id) {
+    return this.getNotes()
+      .then((notes) => notes.filter((note) => note.id !== id))
+      .then((filteredNotes) => this.write(filteredNotes));
+  }
+
+  // check against all of the notes to see which on has the id you're looking to delete
+  // call getNotes() and then filter the results to find the id you're looking for and return
+  // the ones that don't match
+}
+
+module.exports = new Notebook();
--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,61 +1,29 @@
-const { v4: uuidv4 } = require("uuid");
-const express = require("express");
 const router = require("express").Router();
-const path = require("path");
-const fs = require("fs");
-const db = path.join(__dirname, "../db/db.json");
-// const note = { uuid, title, text };
-
+const notebook = require("../db/Notebook");
 // get request for /api/notes
+// remember crud
 // READ
-router.get("/notes", function (req, res) {
+router.get("/notes", (req, res) => {
+  notebook
+    .getNotes()
+    .then((notes) => res.json(notes))
+    .catch((err) => res.status(500).json(err));
   console.log("RETRIEVED NOTES");
-  res.sendFile(db);
 });
 
 // CREATE
 router.post("/notes", (req, res) => {
-  // let notes = JSON.parse(fs.readFileSync(db));
-  const newNote = req.body;
-  const notesArray = [];
-  notesArray = JSON.parse(fs.readFileSync(db));
-  newNote.id = uuidv4();
-  notesArray.push(newNote);
-  // return string and write file
-  const notesString = JSON.stringify(notesArray);
-  fs.writeFile(db, notesString, (err) => {
-    if (err) {
-      throw err;
-    }
-  });
-  res.json(notesString);
+  notebook
+    .addNotes(req.body)
+    .then(() => res.json(note))
+    .catch((err) => res.status(500).json(err));
   console.log("NOTE POSTED");
 });
 router.delete("/notes/:id", (req, res) => {
-  //   // delete request for /api/notes/:id
-  fs.readFile(db, (err, data) => {
-    if (err) {
-      throw err;
-    }
-  });
-  //   let notesArray = [];
-  //   let newNote = req.body;
-  //   fs.readFile(db, (err, data) => {
-  //     if (err) {
-  //       throw err;
-  //     }
-  //     notesArray = JSON.parse(data);
-  //     // filter through the notes to delete items by id
-  //   });
-  //   const filterToDelete = notesArray.filter((note) => {
-  //     return note.id !== req.params.id;
-  //   });
-  //   fs.writeFile(db, JSON.stringify(filterToDelete), (err) => {
-  //     if (err) {
-  //       throw err;
-  //     }
-  //   });
-  //   //  return notes array after user note is deleted
-  //   res.json(newNote);
+  notebook
+    .deleteNotes(req.params.id)
+    .then(() => res.json({ ok: true }))
+    .catch((err) => res.status(500).json(err));
+  console.log("NOTE DELETED");
 });
 module.exports = router;
