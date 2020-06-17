d69dec072eb445bfeb48021dd3789563fc4e07c5
Fleshborne <blakeathompson94@gmail.com>
2020-06-17 18:29:30 -0400

    heroku issues

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -2,18 +2,15 @@
 const path = require("path");
 const router = require("express").Router();
 // get /notes route
-const sendHtml = (res, filename) => {
-  return res.sendFile(path.join(__dirname, "..", "public", filename));
-};
 router.get("/notes", (req, res) => {
   // sendFile()- directory, filename you want to display
-  return sendHtml(res, "notes.html");
+  res.sendFile(path.join(__dirname, "../public/notes.html"));
 });
 
 // get "*" route
 router.get("*", (req, res) => {
   // sendFile()- directory, filename you want to display
-  return sendHtml(res, "index.html");
+  res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 // export the router
