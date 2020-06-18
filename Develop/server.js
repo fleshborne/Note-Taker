f5c9e52f890c37d83c57252bf74e46d4e2cb4d65
Fleshborne <blakeathompson94@gmail.com>
2020-06-17 20:14:08 -0400

    heroku issues

--- Develop/server.js
+++ Develop/server.js
@@ -1,22 +1,24 @@
-// require express
 const express = require("express");
-const app = express();
-// set the port
-const PORT = process.env.PORT || 3000;
-// require htmlRoutes
 const htmlRoutes = require("./routes/htmlRoutes");
-// require apiRoutes
 const apiRoutes = require("./routes/apiRoutes");
 
+// set the port
+const app = express();
+const PORT = process.env.PORT || 3000;
+
 // Sets up the Express app to handle data parsing
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 // set the routes
-app.use(express.static("public"));
 app.use("/api", apiRoutes);
 app.use("/", htmlRoutes);
 
+// Express only serves static assets in production
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 // start the server
 
 app.listen(PORT, () =>
